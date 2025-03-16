bb51ef47e1fa9bca5511ffe588c2e68c1b8ae452
obe <olivb40@gmail.com>
2025-03-16 17:40:50 +0100

    Add input translation and delete puzzle resolution in frontend

--- src/components/AttemptForm/AttemptForm.tsx
+++ src/components/AttemptForm/AttemptForm.tsx
@@ -21,14 +21,14 @@ export default function AttemptForm({
           type="text"
           value={attempt}
           onChange={(e) => setAttempt(e.target.value)}
-          placeholder="Entrez votre attempt..."
+          placeholder="Enter your attempt..."
           className="p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-300 w-full"
         />
         <button
           type="submit"
           className="bg-gray-600 cursor-pointer text-white py-2 px-4 rounded hover:bg-gray-500 transition-colors"
         >
-          Try
+          Submit
         </button>
       </form>
       {error && <p className="text-red-400 text-center">{error}</p>}
